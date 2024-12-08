f2b84795de21cb12c4b2cc4fefa05bbdb6030af8
Saba3939 <kents3939@gmail.com>
2024-12-08 14:29:36 +0900

    chage:ホーム画面をログインに対応させました

--- Neotama-app/src/App.jsx
+++ Neotama-app/src/App.jsx
@@ -1,11 +1,19 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "@/lib/firebase";
 import Login from "@/components/Login";
+import Home from "@/components/Home";
 
 function App() {
+	const [user] = useAuthState(auth);
 	return (
-		<div className='App'>
-			<Login />
-		</div>
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/login' element={user ? <Navigate to='/' /> : <Login />} />
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
